2ab3ad48a3ecd56e2b93a36571c7e8ae42289bef
Tiina Pennala <pennala.tiina@gmail.com>
2018-06-06 12:38:01 +0300

    Completed Iteration D1: Finding a Cart

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180604100033) do
+ActiveRecord::Schema.define(version: 20180606093027) do
+
+  create_table "carts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "title"
