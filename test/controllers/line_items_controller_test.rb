71f09434fcff3a9ec71ba4a0cbd5548ab37f0a7f
Tiina Pennala <pennala.tiina@gmail.com>
2018-06-07 09:39:07 +0300

    Completed Iteration F3: Highlighting Changes

--- test/controllers/line_items_controller_test.rb
+++ test/controllers/line_items_controller_test.rb
@@ -49,4 +49,15 @@ class LineItemsControllerTest < ActionDispatch::IntegrationTest
 
     assert_redirected_to line_items_url
   end
+
+  test "should create line_item via ajax" do
+    assert_difference('LineItem.count') do
+      post line_items_url, params: { product_id: products(:ruby).id },
+      xhr: true
+    end
+
+    assert_response :success
+    assert_match /<tr class=\\"line-item-highlight/, @response.body
+  end
+
 end
