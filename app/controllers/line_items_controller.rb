6d3a091844e20de63ad89fbae253b46bc099a1e4
Tiina Pennala <pennala.tiina@gmail.com>
2018-06-07 09:09:12 +0300

    Completed Iteration F2: Creating an Ajax-Based Cart

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -28,10 +28,10 @@ class LineItemsController < ApplicationController
   def create
     product = Product.find(params[:product_id])
     @line_item = @cart.add_product(product)
-
     respond_to do |format|
       if @line_item.save
         format.html { redirect_to store_index_url }
+        format.js
         format.json { render :show, status: :created, location: @line_item }
       else
         format.html { render :new }
---
+++ app/views/line_items/create.js.coffee
@@ -0,0 +1,4 @@
+cart = document.getElementById("cart")
+cart.innerHTML = "<%= j render(@cart) %>"
+# console.log(cart)
+# console.log(cart.innerHTML)
