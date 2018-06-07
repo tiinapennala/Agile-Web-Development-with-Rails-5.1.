2f64d8deb01c09053236f44abef9bcfa4d32ab92
Tiina Pennala <pennala.tiina@gmail.com>
2018-06-07 13:02:51 +0300

    Task G complete & completed Iteration G2: Atom Feeds (issues with atom feeds)

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -66,14 +66,23 @@ class ProductsController < ApplicationController
     end
   end
 
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_product
-      @product = Product.find(params[:id])
+  def who_bought
+    @product = Product.find(params[:id])
+    @latest_order = @product.orders.order(:updated_at).last
+    if stale?(@latest_order)
+      respond_to do |format|
+        format.atom
+      end
     end
+  end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def product_params
-      params.require(:product).permit(:title, :description, :image_url, :price)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  private def set_product
+    @product = Product.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  private def product_params
+    params.require(:product).permit(:title, :description, :image_url, :price)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
   resources :carts
   root 'store#index', as: 'store_index'
 
-  resources :products
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :products do
+    get :who_bought, on: :member
+  end
+
+  # For details on the DSL available within this file, see
+  # http://guides.rubyonrails.org/routing.html
 end
