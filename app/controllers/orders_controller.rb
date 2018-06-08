605f6bd01397cefb6253f5137c0c2990104269b9
Tiina Pennala <pennala.tiina@gmail.com>
2018-06-08 10:24:05 +0300

    Completed rest of Iteration H1: Adding Fields Dynamically to a Form (Dynamically Replacing Components Based on User Actions)

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -67,20 +67,31 @@ class OrdersController < ApplicationController
     end
   end
 
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_order
-      @order = Order.find(params[:id])
+  def pay_type_params
+    if order_params[:pay_type] == "Credit Card"
+      params.require(:order).permit(:credit_card_number, :expiration_date)
+    elsif order_params[:pay_type] == "Check"
+      params.require(:order).permit(:routing_number, :account_number)
+    elsif order_params[:pay_type] == "Purchase Order"
+      params.require(:order).permit(:po_number)
+    else
+      {}
     end
+  end
+
+  private def set_order
+      @order = Order.find(params[:id])
+  end
 
     # Never trust parameters from the scary internet, only allow the white list through.
-    def order_params
+  private def order_params
       params.require(:order).permit(:name, :address, :email, :pay_type)
-    end
+  end
 
-    def ensure_cart_isnt_empty
+  private def ensure_cart_isnt_empty
       if @cart.line_items.empty?
         redirect_to store_index_url, notice: 'Your cart is empty'
       end
-    end
+  end
+
 end
--- app/javascript/PayTypeSelector/index.jsx
+++ app/javascript/PayTypeSelector/index.jsx
@@ -1,18 +1,46 @@
 import React from 'react'
 
+import NoPayType            from './NoPayType';
+import CreditCardPayType    from './CreditCardPayType';
+import CheckPayType         from './CheckPayType';
+import PurchaseOrderPayType from './PurchaseOrderPayType';
+
 class PayTypeSelector extends React.Component {
-  render() {
-    return (
-      <div className="field">
-        <label htmlFor="order_pay_type">Pay type</label>
-        <select id="pay_type" name="order[pay_type]">
-          <option value="">Select a payment method</option>
-          <option value="Check">Check</option>
-          <option value="Credit card">Credit card</option>
-          <option value="Purchase order">Purchase order</option>
-        </select>
-      </div>
-    );
+
+  constructor(props) {
+    super(props);
+    this.onPayTypeSelected = this.onPayTypeSelected.bind(this);
+    this.state = { selectedPayType: null };
   }
+
+  onPayTypeSelected(event) {
+    this.setState({ selectedPayType: event.target.value });
+    console.log(this.state.selectedPayType);
+  }
+
+  render() {
+    let PayTypeCustomComponent = NoPayType;
+    if (this.state.selectedPayType == "Credit card") {
+      PayTypeCustomComponent = CreditCardPayType;
+    } else if (this.state.selectedPayType == "Check") {
+      PayTypeCustomComponent = CheckPayType;
+    } else if (this.state.selectedPayType == "Purchase order") {
+      PayTypeCustomComponent = PurchaseOrderPayType;
+    }
+      return (
+        <div>
+          <div className="field">
+            <label htmlFor="order_pay_type">Pay type</label>
+            <select id="pay_type" onChange={this.onPayTypeSelected} name="order[pay_type]">
+              <option value="">Select a payment method</option>
+              <option value="Check">Check</option>
+              <option value="Credit card">Credit card</option>
+              <option value="Purchase order">Purchase order</option>
+            </select>
+          </div>
+          <PayTypeCustomComponent />
+        </div>
+      );
+    }
 }
 export default PayTypeSelector
