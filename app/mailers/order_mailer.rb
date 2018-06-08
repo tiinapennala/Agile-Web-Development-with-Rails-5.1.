34efa03cf21aafcef5f2324e2781be6e90a6c4e0
Tiina Pennala <pennala.tiina@gmail.com>
2018-06-08 13:19:27 +0300

    Partly completed Iteration I1: Sending Confirmation Emails (Email Templates)

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -6,10 +6,10 @@ class OrderMailer < ApplicationMailer
   #
   #   en.order_mailer.received.subject
   #
-  def received
-    @greeting = "Hi"
+  def received(order)
+    @order = order
 
-    mail to: "to@example.org"
+    mail to: order.email, subject: 'Pragmatic Store Order Confirmation'
   end
 
   # Subject can be set in your I18n file at config/locales/en.yml
