bef3e4dabd3ad03a8ce116c486e3168ae9f4313a
Tiina Pennala <pennala.tiina@gmail.com>
2018-06-08 13:17:21 +0300

    Partly completed Iteration I1: Sending Confirmation Emails (Sending Email)

---
+++ app/mailers/order_mailer.rb
@@ -0,0 +1,25 @@
+class OrderMailer < ApplicationMailer
+  default from: 'Sam Ruby <depot@example.com>'
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.order_mailer.received.subject
+  #
+  def received
+    @greeting = "Hi"
+
+    mail to: "to@example.org"
+  end
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.order_mailer.shipped.subject
+  #
+  def shipped
+    @greeting = "Hi"
+
+    mail to: "to@example.org"
+  end
+end
---
+++ test/mailers/order_mailer_test.rb
@@ -0,0 +1,20 @@
+require 'test_helper'
+
+class OrderMailerTest < ActionMailer::TestCase
+  test "received" do
+    mail = OrderMailer.received
+    assert_equal "Received", mail.subject
+    assert_equal ["to@example.org"], mail.to
+    assert_equal ["from@example.com"], mail.from
+    assert_match "Hi", mail.body.encoded
+  end
+
+  test "shipped" do
+    mail = OrderMailer.shipped
+    assert_equal "Shipped", mail.subject
+    assert_equal ["to@example.org"], mail.to
+    assert_equal ["from@example.com"], mail.from
+    assert_match "Hi", mail.body.encoded
+  end
+
+end
