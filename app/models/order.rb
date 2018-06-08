71093f13449e7a2fbb1dd94f7f7e651264095fbd
Tiina Pennala <pennala.tiina@gmail.com>
2018-06-08 15:18:13 +0300

    Partly completed Iteration I2: Connecting to a Slow Payment Processor with Active Job (Moving Logic Into the Model)

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,9 @@
+require 'active_model/serializers/xml'
+require 'pago'
+
 class Order < ApplicationRecord
+  include ActiveModel::Serializers::Xml
+
   enum pay_type: {
     "Check" => 0,
     "Credit card" => 1,
@@ -16,4 +21,36 @@ class Order < ApplicationRecord
     end
   end
 
+  def charge!(pay_type_params)
+    payment_details = {}
+    payment_method = nil
+
+    case pay_type
+    when "Check"
+      payment_method = :check
+      payment_details[:routing] = pay_type_params[:routing_number]
+      payment_details[:account] = pay_type_params[:account_number]
+    when "Credit card"
+      payment_method = :credit_card
+      month,year = pay_type_params[:expiration_date].split(//)
+      payment_details[:cc_num] = pay_type_params[:credit_card_number]
+      payment_details[:expiration_month] = month
+      payment_details[:expiration_year] = year
+    when "Purchase order"
+      payment_method = :po
+      payment_details[:po_num] = pay_type_params[:po_number]
+    end
+
+    payment_result = Pago.make_payment(
+      order_id: id,
+      payment_method: payment_method,
+      payment_details: payment_details
+    )
+
+    if payment_result.succeeded?
+      OrderMailer.received(self).deliver_later
+    else
+      raise payment_result.error
+    end
+  end
 end
