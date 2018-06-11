d098943b1f2eb7be5c2538c9571844884a9f6e58
Tiina Pennala <pennala.tiina@gmail.com>
2018-06-11 11:46:25 +0300

    Completed Iteration J3: Limiting Access

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,4 +1,5 @@
 class LineItemsController < ApplicationController
+  skip_before_action :authorize, only: [:create, :destroy]
   include CurrentCart
   before_action :set_cart, only: [:create]
   before_action :set_line_item, only: [:show, :edit, :update, :destroy]
