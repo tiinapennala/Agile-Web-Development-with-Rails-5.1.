0085728dbe832e1dc0e07da02e8e929553751f4c
Tiina Pennala <pennala.tiina@gmail.com>
2018-06-11 10:27:38 +0300

    Completed Iteration J1: Adding Users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180607073428) do
+ActiveRecord::Schema.define(version: 20180611070819) do
 
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -43,6 +43,13 @@ ActiveRecord::Schema.define(version: 20180607073428) do
     t.text "description"
     t.string "image_url"
     t.decimal "price", precision: 8, scale: 2
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
